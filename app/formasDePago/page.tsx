ad2d78f41b82a52ca1717147bd10155f18ebf046
Rickypg711 <77697094+Rickypg711@users.noreply.github.com>
2023-06-29 13:15:42 -0700

    todobien

--- app/formasDePago/page.tsx
+++ app/formasDePago/page.tsx
@@ -98,7 +98,6 @@ export default function FormasDePago() {
     var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
     return !m ? null : "(" + m[1] + ") " + m[2] + "-" + m[3];
   };
-
   return (
     <div className=" flex flex-col min-h-screen ">
       <Navbar />
