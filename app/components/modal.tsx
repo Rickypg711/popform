2963e2652754836411f5e14d215f2ac2a92d5e0f
Rickypg711 <77697094+Rickypg711@users.noreply.github.com>
2023-06-30 08:50:23 -0700

    modal

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -26,7 +26,7 @@ export default function Modal({
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center mx-3"
+      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center mx-3 z-20"
       id="wrapper"
       onClick={handleClose}
     >
