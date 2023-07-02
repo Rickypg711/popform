5b0cccd03f527e031382df227f70fa70f71b6056
Rickypg711 <77697094+Rickypg711@users.noreply.github.com>
2023-07-02 11:45:36 -0700

    moda??

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -1,4 +1,4 @@
-import { Children, ReactNode, MouseEvent } from "react";
+import { ReactNode, MouseEvent } from "react";
 
 interface ModalProps {
   isVisible: boolean;
@@ -26,19 +26,14 @@ export default function Modal({
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center my-24 mx-3 z-20"
+      className="fixed inset-0 flex justify-center items-center z-20"
       id="wrapper"
       onClick={handleClose}
     >
-      <div className="w-full max-w-md">
-        {/* <button
-          className="text-white text-xl place-self-end"
-          onClick={() => onClose()}
-        >
-          x
-        </button> */}
-        <div className="bg-yellow-300 backdrop-blur-sm p-2 bg-opacity-25 rounded">
-          {children}
+      <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+        <div className="bg-black bg-opacity-25 absolute inset-0"></div>
+        <div className="bg-yellow-300 p-2 bg-opacity-25 rounded max-w-md w-full overflow-auto">
+          <div className="relative">{children}</div>
         </div>
       </div>
     </div>
